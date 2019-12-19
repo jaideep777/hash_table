6d2dd68378ae7dd3737cc8f1d69da01542a2914f
jaideep777 <jaideep777@gmail.com>
2019-12-19 18:17:42 +0100

    Completed class-less hashmap - seems to be passing all tests

--- test_hashtable.cpp
+++ test_hashtable.cpp
@@ -9,153 +9,6 @@ using namespace std;
 
 int main(){
 
-// OTHER TESTS
-
-//	const int len = 16;	// better be a prime numnber		
-//	HashNode<int, float> ht[len];
-	
-//	for (auto key : {0,1,7,8,9,14,21,22,6}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-//	
-//	for (auto key : {0,1,7,14,21,22,28,42,6}){
-//		int id = hash_find(key, ht, len);
-//	}
-
-//	for (auto key : {14,21,22}){
-//		hash_delete(key, ht, len);
-//	}
-
-//	hash_print(ht, len);
-//	
-//	for (auto key : {11,33,44,55,66}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-//	for (auto key : {11,33,44,55,66}){
-//		hash_delete(key, ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-
-//	for (auto key : {44}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-
-	// SYSTEMATICALLY TEST DELETE
-	
-//	hash_clear(ht, len);
-//	hash_print(ht, len);
-
-//	for (auto key : {0,11,22,33,44}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-//	
-//	for (auto key : {1,12,23,34,45}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-//	for (auto key : {9}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-//	for (auto key : {0,22,44}){
-//		hash_delete(key, ht, len);
-//	}
-
-//	hash_print(ht, len);
-//	
-//	for (auto key : {0,11,22,33,44,55}){
-//		int id = hash_find(key, ht, len);
-//	}
-
-//	for (auto key : {0}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-//	
-//	for (auto key : {1,2,3,4,5,6,7,8,9,10}){
-//		hash_find(key, ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-//	
-//	for (auto key : {44,22}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-
-//	for (auto key : {9,12,34,11,33,45}){
-//		hash_delete(key, ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-
-//	for (auto key : {33, 77, 99, 13}){
-//		hash_insert(key, float(key/10.f), ht, len);
-//	}
-
-//	hash_print(ht, len);
-
-//	for (auto key : {99, 110}){
-//		hash_find(key, ht, len);
-//	}
-
-	// 2. TEST DUPLICATE KEYS INSERTION
-
-//	size_t a = -1;
-//	cout << "-1 size_t = " << a << endl;
-
-//	const int len = 16;	// better be a prime numnber		
-//	HashNode<int, float> ht[len];
-//	
-//	for (auto key : {0,16,32,64}){
-//		hash_insert(key, float(key/10.f), ht, len);
-////		hash_print(ht, len);
-//	}
-
-//	hash_print(ht, len);
-//	
-//	for (auto key : {0,16,32,64,65,66}){
-//		int id = hash_find(key, ht, len).id;
-//	}
-
-//	for (auto key : {16}){
-//		hash_delete(key, ht, len);
-//	}
-
-//	hash_print(ht, len);
-//	
-//	for (auto key : {0,16,32,64,128,256}){
-//		int id = hash_find(key, ht, len).id;
-//	}
-
-
-//	for (auto key : {32, 128, 256, 256}){
-//		hash_insert(key, float(key/100.f), ht, len);
-////		hash_print(ht, len);
-//	}
-
-//	hash_print(ht, len);
 
 	// 3. RIGOROUS TESTING 
 	
@@ -397,5 +250,152 @@ int main(){
 
 
 
+// OTHER TESTS
+
+//	const int len = 16;	// better be a prime numnber		
+//	HashNode<int, float> ht[len];
+	
+//	for (auto key : {0,1,7,8,9,14,21,22,6}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+//	
+//	for (auto key : {0,1,7,14,21,22,28,42,6}){
+//		int id = hash_find(key, ht, len);
+//	}
+
+//	for (auto key : {14,21,22}){
+//		hash_delete(key, ht, len);
+//	}
+
+//	hash_print(ht, len);
+//	
+//	for (auto key : {11,33,44,55,66}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+//	for (auto key : {11,33,44,55,66}){
+//		hash_delete(key, ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+
+//	for (auto key : {44}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+
+	// SYSTEMATICALLY TEST DELETE
+	
+//	hash_clear(ht, len);
+//	hash_print(ht, len);
+
+//	for (auto key : {0,11,22,33,44}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+//	
+//	for (auto key : {1,12,23,34,45}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+//	for (auto key : {9}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+//	for (auto key : {0,22,44}){
+//		hash_delete(key, ht, len);
+//	}
+
+//	hash_print(ht, len);
+//	
+//	for (auto key : {0,11,22,33,44,55}){
+//		int id = hash_find(key, ht, len);
+//	}
+
+//	for (auto key : {0}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+//	
+//	for (auto key : {1,2,3,4,5,6,7,8,9,10}){
+//		hash_find(key, ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+//	
+//	for (auto key : {44,22}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+
+//	for (auto key : {9,12,34,11,33,45}){
+//		hash_delete(key, ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+
+//	for (auto key : {33, 77, 99, 13}){
+//		hash_insert(key, float(key/10.f), ht, len);
+//	}
+
+//	hash_print(ht, len);
+
+//	for (auto key : {99, 110}){
+//		hash_find(key, ht, len);
+//	}
+
+	// 2. TEST DUPLICATE KEYS INSERTION
+
+//	size_t a = -1;
+//	cout << "-1 size_t = " << a << endl;
+
+//	const int len = 16;	// better be a prime numnber		
+//	HashNode<int, float> ht[len];
+//	
+//	for (auto key : {0,16,32,64}){
+//		hash_insert(key, float(key/10.f), ht, len);
+////		hash_print(ht, len);
+//	}
+
+//	hash_print(ht, len);
+//	
+//	for (auto key : {0,16,32,64,65,66}){
+//		int id = hash_find(key, ht, len).id;
+//	}
+
+//	for (auto key : {16}){
+//		hash_delete(key, ht, len);
+//	}
+
+//	hash_print(ht, len);
+//	
+//	for (auto key : {0,16,32,64,128,256}){
+//		int id = hash_find(key, ht, len).id;
+//	}
+
+
+//	for (auto key : {32, 128, 256, 256}){
+//		hash_insert(key, float(key/100.f), ht, len);
+////		hash_print(ht, len);
+//	}
+
+//	hash_print(ht, len);
 
 
